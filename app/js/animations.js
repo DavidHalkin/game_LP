ac03519eb1eee892e95c28c65e8b02824d764551
Halkin David <halkin.david@gmail.com>
2021-09-16 15:35:51 +0300

    sdf

---
+++ app/js/animations.js
@@ -0,0 +1,19 @@
+new RoadMap();
+
+function RoadMap() {
+
+    const mask = document.querySelector('#pathmask');
+    const pathLength = 5280;
+
+    let progress = 0;
+
+    let time = setInterval(() => {
+
+        const maskLength = pathLength * (1 - progress);
+        mask.setAttribute('stroke-dashoffset', maskLength);
+        progress += 0.001;
+        if(progress >= 1) clearInterval(time);
+        
+    }, 16);
+
+}
