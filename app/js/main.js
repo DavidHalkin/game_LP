5bd0fa95e5f32c492b8734635642d7a6db8240ee
Aleksei <nordskill.design@gmail.com>
2021-09-17 14:25:53 +0200

    dropped jQuery dependency

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,14 @@
-$(document).ready(function(){
-	$(window).scroll(function(){
-	    if ($(window).scrollTop() >= 50) {
-	        $('.header_js').addClass('header_scrolled');
-	    }
-	    else {
-	        $('.header_js').removeClass('header_scrolled');
-	    }
-	});
-	// mob menu
-	$(".btn_mob_js").click(function(){
-        $(".nav_holder_js").toggleClass("opened");
-        $(this).toggleClass("opened");
-    });
+document.addEventListener('scroll', () => {
+	const header = $('.header_js');
+	if (window.scrollY >= 50) return header.classList.add('header_scrolled');
+	header.classList.remove('header_scrolled');
 });
+// mob menu
+$('.btn_mob_js').onclick = event => {
+	$('.nav_holder_js').classList.toggle('opened');
+	event.target.classList.toggle('opened');
+};
+
+function $(selector) {
+	return document.querySelector(selector);
+}
