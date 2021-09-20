1d99501c407ae1d71df38fb20b0cbbe27ecf5620
Aleksei <nordskill.design@gmail.com>
2021-09-20 03:23:54 +0200

    env: webpack added

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = async function() {
+
+    return {
+        mode: 'production',
+        entry: __dirname + '/app/js/main.js',
+        output: {
+            path: __dirname + '/app/js',
+            filename: 'main.min.js'
+        },
+        optimization: {
+            usedExports: true,
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: false,
+                    terserOptions: {
+                        // ecma: undefined,
+                        // warnings: false,
+                        // parse: {},
+                        // compress: {},
+                        // mangle: true, // Note `mangle.properties` is `false` by default.
+                        // module: false,
+                        output: {
+                            comments: false
+                        },
+                        // toplevel: false,
+                        // nameCache: null,
+                        // ie8: false,
+                        // keep_classnames: undefined,
+                        // keep_fnames: false,
+                        // safari10: true
+                    },
+                })
+            ]
+        }
+    }
+
+};
